bcb42701a8aeca460bcfc9ec523ef7f6e63ddf82
Dongyu Zheng <garydzheng@gmail.com>
2017-11-14 14:03:37 -0500

    no prempt, timeout retry

--- backend/master.sh
+++ backend/master.sh
@@ -2,5 +2,12 @@
 
 
 echo $(date)>> /tmp/keepalived.log 2>&1
-aws ec2 disassociate-address --public-ip EIP>> /tmp/keepalived.log 2>&1
-aws ec2 associate-address --public-ip EIP --instance-id INSTANCE_ID>> /tmp/keepalived.log 2>&1
+
+# https://unix.stackexchange.com/questions/82598/how-do-i-write-a-retry-logic-in-script-to-keep-retrying-to-run-it-upto-5-times/82610
+n=0
+until [ $n -ge 10 ]
+do
+  timeout 5 aws ec2 associate-address --public-ip EIP --instance-id INSTANCE_ID>> /tmp/keepalived.log 2>&1 && break
+  n=$[$n+1]
+  sleep 1
+done
